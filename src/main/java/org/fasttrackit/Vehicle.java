6aff3ac382e837940621098a5baccddc2575ab26
Abel Bota <botaabel7@gmail.com>
2019-01-17 19:59:13 +0200

    studied arithmetic operators

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -2,36 +2,52 @@ package org.fasttrackit;
 
 public class Vehicle {
 
-    public static int  vehicleCount = 4;
+    public static int vehicleCount = 4;
 
-  private String name;
-  private String color;
-  private double mileage;
+    private String name;
+    private String color;
+    private double mileage;
+    private double fuelLevel;
+    private double totalTraveledDistance;
+
 
     public Vehicle() {
         vehicleCount++;
     }
 
-    public  double accelerate(double speed, double durationInHours){
+    public double accelerate(double speed, double durationInHours) {
         System.out.println(name + "accelerated with" + speed + "km/h for" + durationInHours + "hours.");
 
         double traveledDistance = speed * durationInHours;
 
         System.out.println("Traveled distance:" + traveledDistance);
 
+//        same as : totalTraveledDistance = totalTraveledDistance + traveledDistance;
+        totalTraveledDistance += traveledDistance;
+        System.out.println("Total traveled distance: " + totalTraveledDistance);
+
+        double spentFuel = traveledDistance * mileage / 100;
+//        fuelLevel = fuelLevel - spentFuel
+        fuelLevel -= spentFuel;
+        System.out.println("Remaining fuel:" + fuelLevel);
+
+
+
         return traveledDistance;
 
     }
-//    example of method overloading
-    public double accelerate(double speed ) {
+
+    //    example of method overloading
+    public double accelerate(double speed) {
         return accelerate(speed, 1);
 
     }
-     protected Vehicle clone(){
+
+    protected Vehicle clone() {
         Vehicle vehicle = new Vehicle();
 //        copy proprietes from current object or simply call this.clone();
-         return vehicle;
-     }
+        return vehicle;
+    }
 
 
     public String getName() {
@@ -56,6 +72,22 @@ public class Vehicle {
 
     public void setMileage(double mileage) {
         this.mileage = mileage;
+    }
+
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public void setFuelLevel(double fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public double getTotalTraveledDistance() {
+        return totalTraveledDistance;
+    }
+
+    public void setTotalTraveledDistance(double totalTraveledDistance) {
+        this.totalTraveledDistance = totalTraveledDistance;
     }
 
     @Override
